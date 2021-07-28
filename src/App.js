966934c682411dc1ab9fc13db3024c2c10f99702
Haseeb-Iqbal <a.haseeb19@gmail.com>
2021-07-28 21:17:39 +0500

    adding header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/Shop/Shop.component';
+import Header from './component/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
